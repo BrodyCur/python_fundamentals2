cc3960fd5fa74de6fe991b6b765fb98b2568ed7d
Brody Currie <brodycurrie@gmail.com>
2019-05-07 17:37:08 -0400

    Went through and removed the variables/code that named my runners. And fixed some naming conventions. My code is more condensed now.

--- exercise7.py
+++ exercise7.py
@@ -1,27 +1,23 @@
-runner1 = input("Runner 1 name?: ")
-runner2 = input("Runner 2 name?: ")
-runner3 = input("Runner 3 name?: ")
-
-def runner_speed(runner):
-    distance = float(input(f"How far did {runner} run?"))
-    mins = float(input(f"How long did it take in mins?"))
+def runner_data(id):
+    distance = float(input(f"How far did runner {id} run?\n"))
+    mins = float(input(f"How long did it take in mins?\n"))
 
     secs = mins * 60
     return distance / secs
 
-speed1 = runner_speed(runner1)
-speed2 = runner_speed(runner2)
-speed3 = runner_speed(runner3)
+speed1 = runner_data(1)
+speed2 = runner_data(2)
+speed3 = runner_data(3)
 
 def determine_winner():
     if speed1 > speed2 and speed1 > speed3:
-        print("{} was the fastest at {} m/s".format(runner1, speed1))
+        print(f"Person 1 was the fastest at {speed1} m/s")
     elif speed2 > speed1 and speed2 > speed3:
-        print("{} was the fastest at {} m/s".format(runner2, speed2))
+        print(f"Person 2 was the fastest at {speed2} m/s")
     elif speed3 > speed1 and speed3 > speed2:
-        print("{} was the fastest at {} m/s".format(runner3, speed3))
+        print(f"Person 3 was the fastest at {speed3} m/s")
     elif speed1 == speed2 and speed2 == speed3:
-        print("Everyone tied at {} m/s".format(speed1))
+        print(f"Everyone tied at {speed1} m/s")
     else:
         print("Well done everyone!")
 
