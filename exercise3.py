27207201e748d0b3783674789981eef37b54eec4
Brody Currie <brodycurrie@gmail.com>
2019-05-07 14:43:03 -0400

    completed exercise3

---
+++ exercise3.py
@@ -0,0 +1,11 @@
+def is_even(num):
+    if num % 2 == 0 :
+        return True
+    else:
+        return False
+
+print(is_even(2))
+print(is_even(654))
+print(is_even(333))
+print(is_even(403))
+print(is_even(4079325))
