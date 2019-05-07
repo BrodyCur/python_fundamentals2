94102ec7e11f215595ac07f61691289d8c517534
Brody Currie <brodycurrie@gmail.com>
2019-05-07 14:33:27 -0400

    completed exercise2

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+def negative(num):
+    if num < 0:
+        return True
+    else:
+        return False
+
+print(negative(-4))
+print(negative(4))
+print(negative(-34564))
+print(negative(-65873234))
+print(negative(0))
